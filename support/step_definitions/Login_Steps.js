273ac4c3f7889ec2bd2e94e3d8722d02b73e0b58
paulekat <paulekat@gmail.com>
2023-04-03 12:35:42 -0400

    Add files via upload

--- support/step_definitions/Login_Steps.js
+++ support/step_definitions/Login_Steps.js
@@ -18,9 +18,9 @@ When(`I click the Sign In button`, () => {
     cy.get('[type="submit"]').click();
 });
 
-// Then(`Then I should be logged in to my account and land on the Project Feed page`, () => {
-//     cy.contains("Project Feed").should('be.visible');
-// });
+Then(`I am logged in successfully`, () => {
+    cy.get('[data-testid="projects__main-page-title"]').should('have.text', 'Project Feed');
+});
 
 Given("I am logged into my account", function () {
     cy.visit(url);
@@ -40,28 +40,50 @@ When("I click on the Users button", function () {
 });
 
 When("I click on the Invite New Users button", function () {
-    cy.get('.ccb-blue').click();
+    cy.get('.ccb-blue > span').click();
 });
 
-When("I type a first name", function () {
-    cy.get('.sc-bkzrtC > :nth-child(1) > input').type('John');
+// When("I type a first name", function () {
+//     cy.get('.sc-bkzrtC > :nth-child(1) > input').type('John');
+// });
+
+// When("I type a last name", function () {
+//     cy.get('.sc-bkzrtC > :nth-child(2) > input').type('Doe');
+// });
+
+When("I type an {string} and tap enter", (email) => {
+    cy.get('[data-testid="user-invite-email-field"]')
+    .type(email)
+    .type('{enter}');
+})
+
+When('I click the Review Invite button', function ()  { 
+    cy.get('.sc-LzLsD').click();
 });
 
-When("I type a last name", function () {
-    cy.get('.sc-bkzrtC > :nth-child(2) > input').type('Doe');
-});
-
-When("I type an email address", function () {
-    cy.get(':nth-child(3) > input').type('johndoe+65@example.com');
-});
 
 When("I click on the Send Invite button", function () {
-    cy.get('.ccb-blue').click();
+    cy.get('[data-testid="user-invite-review-send"] > .sc-LzLsD').click();
 });
 
-Then("I should see the new user's invite has been created on the Users page", function () {
-    cy.contains('johndoe+65@example.com').should('be.visible');
+// Then("I should see a {string}", (message) => {
+//     cy.get('*[class^="sc-Lz0ms gyHDcw').contains(message);
+// });
+
+When(`I type an invalid password`, () => {
+    cy.get('#user_password').type("Lkab1rd1");
 });
+
+Then(`I should be presented with a signon error message`, () => {
+    cy.get('.errors > p').should('have.text', 'Invalid Email address or password.');
+});
+
+Then(`I should see that the new email address was invited successfully`, () => {
+    cy.get(':nth-child(1) > .jAWvFF > .sc-LzOGD').contains
+});
+
+
+
 
 
 
